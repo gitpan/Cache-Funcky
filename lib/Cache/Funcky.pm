d67ee728fe546ea0490106201075eb656be1eba0
Masahiro Funakoshi <masap@cpan.org>
2007-05-19 17:12:49 +0000

    Import of MASAP/Cache-Funcky-0.0.2 from CPAN.
    gitpan-cpan-distribution: Cache-Funcky
    gitpan-cpan-version:      0.0.2
    gitpan-cpan-path:         MASAP/Cache-Funcky-0.0.2.tar.gz
    gitpan-cpan-author:       MASAP
    gitpan-cpan-maturity:     released

--- lib/Cache/Funcky.pm
+++ lib/Cache/Funcky.pm
@@ -5,7 +5,7 @@ use strict qw( subs );
 use Carp;
 use UNIVERSAL::require;
 
-use version; our $VERSION = qv('0.0.1');
+use version; our $VERSION = qv('0.0.2');
 
 sub setup {
     my $class        = shift;
